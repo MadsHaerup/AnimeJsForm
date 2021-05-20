21049410f6446b0a18d885ff3d8047c3ac363910
Mads Haerup <506094@rts-365.dk>
2021-05-20 12:33:37 +0200

    form

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import anime from 'animejs/lib/anime.es.js';
+
 
 function App() {
+
+  function animate(element){
+    anime({
+      targets: `label[for = "${element}"]`,
+      translateY: -8,
+      translateX: -47,
+      scale: {value:0.8},
+      background: '#97FF97',
+      color: '#899993'
+    })
+  }
+  function leave(element){
+    if(document.querySelector(`#${element}`).value) return;
+    anime({
+      targets:`label[for = "${element}"]`,
+      translateY: 30,
+      scale: {value:1},
+      translateX: 0,
+      background: '#fff',
+      color: '#000'
+    })
+    
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <section>
+     <h1>Contact us</h1>
+     <form onSubmit={e=>e.preventDefault()}>
+       <div className="inputGroup">
+         <label  htmlFor="name">Name</label>
+         <input onFocus={()=>animate("name")} onBlur={()=>leave("name")}  type="text" name="name" id="name" />
+       </div>
+       <div className="inputGroup">
+         <label  htmlFor="phone">Phone</label>
+         <input onFocus={()=>animate("phone")} onBlur={()=>leave("phone")} type="tel" name="phone" id="phone" />
+       </div>
+       <div className="inputGroup">
+         <label  htmlFor="email">Email</label>
+         <input onFocus={()=>animate("email")} onBlur={()=>leave("email")} type="email" name="email" id="email" />
+       </div>
+       <div className="inputGroup">
+         <label  htmlFor="message">Message</label>
+         <textarea onFocus={()=>animate("message")} onBlur={()=>leave("message")} name="message" id="message" />
+       </div>
+
+       <button type="submit" >Send</button>
+       
+     </form>
+   </section>
   );
 }
 
